678b69cb697dd07851783ede69bf0df967996014
SepSarip <63574047+SepSarip@users.noreply.github.com>
2023-02-25 10:55:51 +0700

    Add files via upload

---
+++ APKCaptive.bat
@@ -0,0 +1,2 @@
+REM Batch script to generate signed APK, with captive AIR runtime
+D:\Program Files\AIRSDK_Compiler\bin\adt.bat -package -target apk-captive-runtime -storetype pkcs12 -keystore D:\Kelase\Theory\library\codecanyon-4600694-andaired\Certificate\mycert.p12  -storepass iwan24itb osi.captive.apk application.xml osi.swf
